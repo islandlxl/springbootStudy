e976145c7cf383ba9839874ed030c5083136d1fb
islandlxl <569881564@qq.com>
2020-02-06 16:13:49 +0800

    删除失败的应用

--- spring-boot_shiro/src/main/java/com/islandlxl/springboot_shiro/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.islandlxl.springboot_shiro.service;
-
-
-import com.islandlxl.springboot_shiro.model.User;
-
-public interface UserService {
-
-    User findByUsername(String username);
-}
